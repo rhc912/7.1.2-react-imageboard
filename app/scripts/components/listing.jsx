6d64725d22461dab7e8efdc08582321dc97a1b80
rhc912 <rhc912@gmail.com>
2016-06-28 16:47:13 -0400

    improved images

--- app/scripts/components/listing.jsx
+++ app/scripts/components/listing.jsx
@@ -4,30 +4,65 @@ var $ = require('jquery');
 //var ReactDOM = require('react-dom');
 //
 var Feed = React.createClass({
+  getInitialState: function(){
+    return {showForm: false};
+  },
+  handleClick: function(event){
+    event.preventDefault();
+    var currentDisplay = this.state.showForm;
+    this.setState({showForm: !currentDisplay})
+
+
+  },
   render: function(){
-
-  var images =  this.props.collection.map(function(image){
-    return( <div>
-
-          <li key={image.cid}><img className="images col-xs-10 col-xs-offset-1" src={image.get('url')}/></li>
-            <div className="caption-con col-xs-10 col-xs-offset-1">
+    var textComponent = this.state.showForm;
+    var images =  this.props.collection.map(function(image){
+      return(
+          <li className="col-xs-10 col-xs-offset-1" key={image.cid}>
+            <img className="images" src={image.get('url')}/>
+            <div className="picture-tile caption-con">
               <p>{image.get('title')}</p>
             </div>
-          </div>
+          </li>
         )
-  });
+    });
+
+    var imageForm = this.state.showForm ? <ImageForm  /> : '';
+
     return (
-    <div>
-      <div className="well">
-        <button className="btn add-btn btn-primary"><i className="fa fa-plus" aria-hidden="true"></i></button>
+      <div>
+        <div className="well">
+          <button onClick={this.handleClick} className="btn add-btn btn-primary"><i className="fa fa-plus" aria-hidden="true"></i></button>
 
-      </div>
+        </div>
 
-      <div className="row text-center">
-        {images}
+        {imageForm}
+
+        <div className="row text-center">
+          {images}
+        </div>
       </div>
-      </div>
-    )
+    );
+
+  }
+});
+var ImageForm = React.createClass({
+  render: function(){
+    return (
+      <form onSubmit={this.props.handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="image-item">Add Image</label>
+          <input
+            onChange={this.props.handleChange}
+            type="text"
+            className="form-control"
+            id="image-item"
+            value={this.props.inputText}
+            placeholder="Add URL" />
+        </div>
+        <input type="submit" className="btn btn-success" value="Add"/>
+      </form>
+    );
   }
 });
 var Navigation = React.createClass({
@@ -45,5 +80,6 @@ var Navigation = React.createClass({
 
 module.exports = {
   'Navigation': Navigation,
-  'Feed': Feed
+  'Feed': Feed,
+  'ImageForm': ImageForm
 }
